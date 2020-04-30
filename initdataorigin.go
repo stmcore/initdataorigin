137f48501b8a2ce9c29bb86721acf3c93f3eb660
Wisarut <wisarut.khu@truedigital.com>
2020-04-30 19:33:08 +0700

    edit log

--- initdataorigin.go
+++ initdataorigin.go
@@ -389,7 +389,7 @@ func getRedisJSON(hostname, addr string, rh *rejson.Handler) (server Server, err
 		}
 	}()
 	rh.SetRedigoClient(conn)
-	fmt.Println("executing JSONGET for Redigo Client")
+	glg.Info("executing JSONGET for Redigo Client:", hostname)
 
 	serverJSON, err := redis.Bytes(rh.JSONGet(hostname, "."))
 	if err != nil {
@@ -421,7 +421,7 @@ func (dataori *DataOrigins) Init() {
 		// arrIP := strings.Split(server.IP, ".")
 		// lastTwoIP := strings.Join(arrIP[len(arrIP)-2:], ".")
 
-		glg.Info("Getting connectioncounts from: " + host.Hostname + "(" + host.IP + ")" + " ...")
+		//glg.Info("Getting connectioncounts from: " + host.Hostname + "(" + host.IP + ")" + " ...")
 		// data, err := digest.GetInfo("http://"+server.IP+":8086/connectioncounts", Username, Pattern+lastTwoIP, "GET")
 
 		// if err != nil {
